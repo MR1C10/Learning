dc9f28f85cd3504c66d69324100520fe9b7a486c
MR1C10 <mauriciorodriguesoficial@gmail.com>
2025-02-10 11:45:21 -0300

    Complete list 1

--- Java/lista1/src/Main.java
+++ Java/lista1/src/Main.java
@@ -53,21 +53,92 @@ public class Main {
         //três, leia outro número e some ao seu valor o número dois, some os resultados
         //das operações realizadas com os números lidos , subtrai do resultado o número
         //1 e mostre em vídeo o valor final da operação.
-        System.out.print("digite um numero: ");
-        num = Ler.nextInt();
-        int calc1 = num - 3;
+//        System.out.print("digite um numero: ");
+//        num = Ler.nextInt();
+//        int calc1 = num - 3;
+//
+//        System.out.print("digite outro numero: ");
+//        num2 = Ler.nextInt();
+//        int calc2 = num2 + 2;
+//
+//        int result = calc1 + calc2; // t
+//        int result2 = num + num2 ; // n
+//
+//        int result3 = result + result2; //m
+//
+//        int result4 = result3 - 1;
+//
+//        System.out.println(result4);
 
-        System.out.print("digite outro numero: ");
-        num2 = Ler.nextInt();
-        int calc2 = num2 + 2;
+        //7. Criando uma tabuada. Crie um algoritmo que leia um número e mostre
+        //como resultado a tabuada de multiplicação do número lido.
+//        int a,b,c,d,e,f,g,h,i,j;
+//        System.out.print("Digite o numero que deseja a tabuada: ");
+//        int x = Ler.nextInt();
+//
+//        a = x * 1;
+//        System.out.println(a);
+//        a = x * 2;
+//        System.out.println(a);
+//        a = x * 3;
+//        System.out.println(a);
+//        a = x * 4;
+//        System.out.println(a);
+//        a = x * 5;
+//        System.out.println(a);
+//        a = x * 6;
+//        System.out.println(a);
+//        a = x * 7;
+//        System.out.println(a);
+//        a = x * 8;
+//        System.out.println(a);
+//        a = x * 9;
+//        System.out.println(a);
+//        a = x * 10;
+//        System.out.println(a);
 
-        int result = num + num2;
-        int result2 = calc1 + calc2;
-        int result3 = result + result2;
+        //8. Elaborar um algoritmo que leia três números some 10 ao primeiro, subtraia
+        //cinco do segundo e multiplique o terceiro por dois, após efetuar estas
+        //operações mostre o resultado das operações.
 
-        result3 -= 1;
+//        System.out.print("Digite um numero: ");
+//        num = Ler.nextInt();
+//        System.out.print("Digite outro numero: ");
+//        num2 = Ler.nextInt();
+//        System.out.print("Digite mais um numero: ");
+//        num3 = Ler.nextInt();
+//
+//        num += 10;
+//        num2 -= 5;
+//        num3 *= 2;
+//
+//        System.out.println(num + "\n" + num2 + "\n" + num3 );
 
-        System.out.println(result3);
+        //9. Descrever um algoritmo que leia um número, subtrai do seu valor o numero
+        //três, some o resultado ao número lido, e mostre o valor final da operação.
+
+//        System.out.println("Digite um numero");
+//        num = Ler.nextInt();
+//        num2 = num - 3;
+//        num2 += num;
+//        System.out.println(num2);
+
+        //10. Crie um algoritmo que leia um número, divida o seu valor por dois e atribua o
+        //resultado da operação a duas variáveis diferentes, soma a uma das variáveis o
+        //número três e a outra o número cinco, ao final, mostre o valor das duas
+        //variáveis.
+
+//        System.out.println("Digite um numero: ");
+//        num = Ler.nextInt();
+//        num /= 2;
+//
+//        num2 = num;
+//        num3 = num;
+//
+//        num2 += 3;
+//        num3 += 5;
+//
+//        System.out.println(num2 + "\n" + num3);
 
     }
 }
