5cdf27b560dd5057387e19d4f72f572c8378f4f6
MR1C10 <mauriciorodriguesoficial@gmail.com>
2025-02-11 12:51:25 -0300

    Complete list 3

---
+++ Java/lista3/src/Main.java
@@ -0,0 +1,130 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+        //1. Desenvolva um algoritmo que leia dois números e
+        //mostre o menor.
+
+//        System.out.println("Digite um numero: ");
+//        int num = ler.nextInt();
+//        System.out.println("Digite outro numero: ");
+//        int num1 = ler.nextInt();
+//
+//        if (num < num1){
+//            System.out.println(num);
+//        }
+//        else{
+//            System.out.println(num1);
+//        }
+
+        //2. Elabore um algoritmo que leia dois números, some cinco ao de menor valor,
+        //compare os dois valores e mostre o maior.
+
+//        System.out.println("Digite um numero: ");
+//        int num = ler.nextInt();
+//        System.out.println("Digite outro numero: ");
+//        int num1 = ler.nextInt();
+//
+//        if (num < num1){num+=5;}else{num1+=5;}
+//
+//        if (num > num1){
+//            System.out.println(num);
+//        }
+//        else{
+//            System.out.println(num1);
+//        }
+
+        //3. Desenvolva um algoritmo que leia um número e mostre a seguinte mensagem,
+        //número maior que dez, se ele for maior que dez e menor igual a dez se ele for
+        //menor ou igual a dez.
+
+//        System.out.println("Digite um numero: ");
+//        int num = ler.nextInt();
+//
+//        if (num > 10){
+//            System.out.println("Numero maior que 10");
+//        }
+//        else{
+//            System.out.println("Numero menor ou igual a 10");
+//        }
+
+        //4. Elabore um algoritmo que leia três números e mostre o maior
+
+//        System.out.println("Digite um numero: ");
+//        int num = ler.nextInt();
+//        System.out.println("Digite mais um numero: ");
+//        int num1 = ler.nextInt();
+//        System.out.println("Digite mais um numero: ");
+//        int num2 = ler.nextInt();
+//
+//        if (num > num1 && num > num2){
+//            System.out.println(num + " é maior!");
+//        }
+//        else if (num1 > num && num1 > num2){
+//            System.out.println(num1 + " é maior!");
+//        }
+//        else{
+//            System.out.println(num2 + " é maior");
+//        }
+
+        //5. Elabore um algoritmo que leia três números some cinco ao menor valor e
+        //mostre o resultado.
+
+//        System.out.println("Digite um numero: ");
+//        int num = ler.nextInt();
+//        System.out.println("Digite um numero: ");
+//        int num1 = ler.nextInt();
+//        System.out.println("Digite um numero: ");
+//        int num2 = ler.nextInt();
+//
+//        if (num < num1 && num < num2){
+//            num += 5;
+//            System.out.println(num);
+//        }
+//        else if (num1 < num && num1 < num2){
+//            num1 += 5;
+//            System.out.println(num2);
+//        }
+//        else{
+//            num2 += 5;
+//            System.out.println(num2);
+//        }
+
+        //6. Desenvolva um algoritmo que leia dois números e mostre os números em
+        //ordem crescente.
+
+//        System.out.println("Digite um numero: ");
+//        int num = ler.nextInt();
+//        System.out.println("Digite um numero: ");
+//        int num1 = ler.nextInt();
+//
+//        if (num < num1){
+//            System.out.println(num + "" + num1);
+//        }else{
+//            System.out.println(num1 + "" + num);
+//        }
+
+        //7. Crie um algoritmo que leia três números e mostre seus valores em ordem
+        //crescente.
+
+        System.out.println("Digite um numero: ");
+        int num = ler.nextInt();
+        System.out.println("Digite um numero: ");
+        int num1 = ler.nextInt();
+        System.out.println("Digite um numero: ");
+        int num2 = ler.nextInt();
+
+        //Completar quando chegar em casa
+        if (num < num1 && num < num2 || ){
+            System.out.println(num + "" + num1  );
+        }else{
+            System.out.println(num1 + "" + num);
+        }
+
+
+
+    }
+}
